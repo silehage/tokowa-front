55ab3d0b1933883ae10d7b31a3ea613e86688f7e
silehage <silehwage@gmail.com>
2021-06-11 05:35:10 +0700

    first commit

---
+++ js/12.f482a211.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[12],{"0619":function(n,e,t){"use strict";t.r(e);var l=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",[n._v("Settings")])},s=[],u={},c=u,i=t("2877"),r=Object(i["a"])(c,l,s,!1,null,null,null);e["default"]=r.exports}}]);
